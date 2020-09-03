e08825448b1e70f35fb4160039252b9dfdfe34b8
Elvis Liberato de Barros <liberatov13@gmail.com>
2020-09-02 21:54:29 -0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:house_party/pages/house_party.dart';
+
+void main() {
+  runApp(HouseParty());
+}
---
+++ lib/pages/house_party.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class HouseParty extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'House Party',
+      home: Scaffold(
+        appBar: AppBar(),
+        body: Container(),
+      ),
+    );
+  }
+}
