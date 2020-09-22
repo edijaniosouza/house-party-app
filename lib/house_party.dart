44cbde348702d161b2de111f20c23d7c09e8efa2
Elvis Liberato de Barros <liberatov13@gmail.com>
2020-09-22 20:07:31 -0300

    Revert "Incluindo dependecias do Firebase para Android"
    This reverts commit ef4d384215517541e68651a57da18664cbec6c5e.

--- lib/house_party.dart
+++ lib/house_party.dart
@@ -1,45 +1,11 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:house_party/pages/home/home_page.dart';
 import 'package:house_party/pages/login/login_page.dart';
 
-class HouseParty extends StatefulWidget {
+class HouseParty extends StatelessWidget {
   // This widget is the root of your application.
   @override
-  _HousePartyState createState() => _HousePartyState();
-}
-
-class _HousePartyState extends State<HouseParty> {
-  bool _firebaseInitialized = false;
-  bool _firebaseError = false;
-
-  // Define an async function to initialize FlutterFire
-  void initializeFlutterFire() async {
-    try {
-      // Wait for Firebase to initialize and set `_initialized` state to true
-      await Firebase.initializeApp();
-      setState(() {
-        _firebaseInitialized = true;
-      });
-    } catch(e) {
-      // Set `_error` state to true if Firebase initialization fails
-      setState(() {
-        _firebaseError = true;
-      });
-    }
-  }
-
-  @override
-  void initState() {
-    initializeFlutterFire();
-    super.initState();
-  }
-
-  @override
   Widget build(BuildContext context) {
-    if(_firebaseError) {
-      return ErrorWidget(_firebaseError);
-    }
     return MaterialApp(
       title: 'House Party',
 
