6a29dbf1b2d3b81ad36d31b5e4866985ca799724
Elvis Liberato de Barros <liberatov13@gmail.com>
2020-09-28 02:40:55 -0300

    Criando Singletons de Controllers

--- lib/pages/home/home_controller.dart
+++ lib/pages/home/home_controller.dart
@@ -2,7 +2,18 @@ import 'package:flutter/material.dart';
 
 class HomeController {
 
-  static void navigateToLoginPage(BuildContext context) {
+  static HomeController _instance = HomeController._();
+
+  HomeController._();
+
+  static get getInstace => _instance;
+
+  void navigateToLoginPage(BuildContext context) {
     Navigator.pushNamed(context, '/login');
   }
+
+  void navigateToRegisterPage(BuildContext context) {
+    Navigator.pushNamed(context, '/register');
+  }
+
 }
