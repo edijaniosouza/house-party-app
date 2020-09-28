9e694a6c69ec7b2719c4726572dc43ac5a058a86
Elvis Liberato de Barros <liberatov13@gmail.com>
2020-09-28 02:31:29 -0300

    Inserindo margem ao componente de Input

--- lib/components/input_text.dart
+++ lib/components/input_text.dart
@@ -7,25 +7,45 @@ class InputText extends StatelessWidget {
   final String _hint;
   final TextInputType _keyboardType;
   final bool _isHidden;
+  final double _horizontalMargin;
+  final double _verticalMargin;
 
-  InputText(TextEditingController controller, {String label, String hint, TextInputType keyboardType, bool isHidden})
-  : this._controller = controller,
+  InputText(
+    TextEditingController controller,
+    {
+      String label,
+      String hint,
+      TextInputType keyboardType,
+      bool isHidden,
+      double horizontalMargin,
+      double verticalMargin,
+
+    }
+  ): this._controller = controller,
   this._label = label,
   this._hint = hint,
   this._keyboardType = keyboardType,
-  this._isHidden = isHidden ?? false
+  this._isHidden = isHidden ?? false,
+  this._horizontalMargin = horizontalMargin ?? 0,
+  this._verticalMargin = verticalMargin ?? 0
   ;
   
   @override
   Widget build(BuildContext context) {
-    return TextField(
-      controller: this._controller,
-      keyboardType: this._keyboardType,
-      obscureText: _isHidden,
-      decoration: InputDecoration(
-        labelText: this._label,
-        hintText: this._hint,
-        border: OutlineInputBorder()
+    return Padding(
+      padding: EdgeInsets.symmetric(
+        horizontal: this._horizontalMargin,
+        vertical: this._verticalMargin,
+      ),
+      child: TextField(
+        controller: this._controller,
+        keyboardType: this._keyboardType,
+        obscureText: _isHidden,
+        decoration: InputDecoration(
+          labelText: this._label,
+          hintText: this._hint,
+          border: OutlineInputBorder()
+        ),
       ),
     );
   }
