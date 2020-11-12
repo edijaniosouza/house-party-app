4e90fa3b1c05423c6ff577d0a0bf8cde448bc992
Elvis Liberato de Barros <liberatov13@gmail.com>
2020-11-11 21:13:02 -0300

    Alterando nome do controller da pagina de produtos

---
+++ lib/pages/product/product_list_controller.dart
@@ -0,0 +1,8 @@
+class ProductListController {
+
+  static ProductListController _instance = ProductListController._();
+
+  ProductListController._();
+
+  ProductListController get instace => _instance;
+}
