ca747aa90808dfa984ee7d515571aefc6ec731a6
Elvis Liberato de Barros <liberatov13@gmail.com>
2020-11-24 01:06:59 -0300

    Incluindo imagem ao produto

--- lib/models/product.dart
+++ lib/models/product.dart
@@ -1,3 +1,6 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
 import 'package:house_party/models/category.dart';
 import 'package:house_party/models/provider.dart';
 
@@ -8,14 +11,18 @@ class Product {
   Provider _provider;
   double _price;
   Category _category;
+  Image _image;
 
-  Product(this._id, this._description, this._category, this._price, this._provider, this._ean);
+  Product(this._id, this._description, this._category, this._price, this._provider, this._ean, this._image);
 
   Product.fromJson(Map<String, dynamic> json) {
     this._id = json['id_produto'];
     this._description = json['descricao'];
     this._ean = json['ean'];
     this._category = Category.fromJson(json['categoria']);
+    this._image = !json['imagemProduto'].isEmpty ?
+      Image.memory(base64Decode(json['imagemProduto']))
+      : Image.asset('assets/images/caneca_baloes-vetor.png');
   }
 
   int get id => this._id;
@@ -24,12 +31,14 @@ class Product {
   double get price => this._price;
   Provider get provider => this._provider;
   Category get category => this._category;
+  Image get image => this._image;
 
   Map<String, dynamic> toMap() {
     return {
       'ean'
       'descicao' : this._description,
-      'categoria' : this._category.toMap()
+      'categoria' : this._category.toMap(),
+      'imagem' : base64Encode(this._image.toString().codeUnits)
     };
   }
 }
