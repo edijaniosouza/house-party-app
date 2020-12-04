fe41194048de1d36b2d1b4d973a0f4ba13a6641f
Elvis Liberato de Barros <liberatov13@gmail.com>
2020-12-04 01:03:36 -0300

    Criando tabela de usuário

--- lib/database/database.dart
+++ lib/database/database.dart
@@ -3,11 +3,16 @@ import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
 
 Future<Database> createDataBase() async {
-  return openDatabase(
-    join(await getDatabasesPath(), 'database.db'),
+  String path = join(await getDatabasesPath(), 'database.db');
+  return await openDatabase(
+    path,
     onCreate: (db, version) {
+      print('Criando banco de dados');
+      print('Criando tabela de usuário');
       db.execute(UserDAO.createTable);
+      print('Tabela de usuário criada');
     },
-    version: 1
+    onDowngrade: onDatabaseDowngradeDelete,
+    version: 1,
   );
 }
