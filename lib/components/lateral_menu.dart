781c6eb303cd83b48ea7ac85c7a3e61be5c634f5
Elvis Liberato de Barros <liberatov13@gmail.com>
2020-12-15 01:46:45 -0300

    Incluindo funcionalidade de sign out

---
+++ lib/components/lateral_menu.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:house_party/database/dao/user_dao.dart';
+import 'package:house_party/theme/app_theme.dart';
+
+class LateralMenu extends StatelessWidget {
+  LateralMenu({
+    Key key,
+  }) : super(key: key);
+
+  UserDAO _userDAO = UserDAO();
+
+  singOut(BuildContext context) {
+    _userDAO.deleteAll();
+    Navigator.pushNamedAndRemoveUntil(context, '/', (route) => false);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        children: [
+          DrawerHeader(
+            decoration: BoxDecoration(
+              color: AppTheme.defaultTheme.primaryColor
+            ),
+            child: CircleAvatar(
+              child: Image.asset('assets/images/caneca_baloes-vetor.png'),
+              backgroundColor: AppTheme.defaultTheme.scaffoldBackgroundColor,
+            ),
+          ),
+          Card(
+            elevation: 4,
+            child: ListTile(
+              onTap: () {
+                singOut(context);
+              },
+              leading: FaIcon(FontAwesomeIcons.signOutAlt),
+              title: Text(
+                'Sair',
+                style: TextStyle(
+                  fontSize: 18,
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
