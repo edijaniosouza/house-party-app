6f341b4084ccd042bef89f2da747b6386b5e7f0f
Elvis Liberato de Barros <liberatov13@gmail.com>
2020-12-14 22:05:47 -0300

    Removendo imports de arquivo removido

--- lib/house_party.dart
+++ lib/house_party.dart
@@ -5,7 +5,6 @@ import 'package:house_party/pages/home/home_page.dart';
 import 'package:house_party/pages/login/login_page.dart';
 import 'package:house_party/pages/product/product_list_page.dart';
 import 'package:house_party/pages/register_user/register_user_page.dart';
-import 'package:house_party/pages/company/register_company_page.dart';
 import 'package:house_party/theme/app_theme.dart';
 
 class HouseParty extends StatelessWidget {
@@ -23,7 +22,6 @@ class HouseParty extends StatelessWidget {
         '/' : (context) => HomePage(),
         '/user/login': (context) => UserLoginPage(),
         '/user/register' : (context) => RegisterUserPage(),
-        '/company/register' : (context) => RegisterCompanyPage(),
         '/category' : (context) => CategoryPage(),
         '/product' : (context) => ProductListPage(),
       },
