77fa7d99b67e9e825267c97f0666634dd61be68d
Elvis Liberato de Barros <liberatov13@gmail.com>
2020-12-14 21:58:16 -0300

    Removendo arquivos desnecessários

--- lib/pages/company/register_company_controller.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-
-class RegisterCompanyController {
-  TextEditingController nameInputController = TextEditingController();
-  TextEditingController phoneNumberInputController = TextEditingController();
-  TextEditingController cnpjInputController = TextEditingController();
-  TextEditingController eventsInputController = TextEditingController();
-}
